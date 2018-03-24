fd73ce15a70b25412446e18bb21920448032b78b
ulianandrea <andrea@hi-system.org>
2018-03-24 14:02:14 +0100

    add project page

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page'
+  });
+});
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
